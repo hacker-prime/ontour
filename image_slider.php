a2a761e62043d6c60445da2b5f019ba320dda705
hacker-prime <hacker0prime@gmail.com>
2023-05-07 22:23:39 -0500

    Image Slider - New Images

--- image_slider.php
+++ image_slider.php
@@ -2,35 +2,35 @@
 
     <div id="img-slider" class="img-slider">
         <div class="slide">
-        <img src="assets/images/plane landing in jamaica.jpg" alt="">
+        <img src="assets/images/Plane-Over-Jamaica-Window.jpg" alt="">
         <div class="info">
             <h2>Slide 01</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
         </div>
         <div class="slide active">
-        <img src="assets/images/1.jpg" alt="">
+        <img src="assets/images/plane landing in jamaica.jpg" alt="">
         <div class="info">
             <h2>Slide 02</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
         </div>
         <div class="slide">
-        <img src="assets/images/3.jpg" alt="">
+        <img src="assets/images/airport road for cars.jpg" alt="">
         <div class="info">
             <h2>Slide 03</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
         </div>
         <div class="slide">
-        <img src="assets/images/4.jpg" alt="">
+        <img src="assets/images/Shuttle-Transport-Airport.jpg" alt="">
         <div class="info">
             <h2>Slide 04</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
         </div>
         <div class="slide">
-        <img src="assets/images/5.jpg" alt="">
+        <img src="assets/images/Sangster-International-Airport-Front-View.jpg" alt="">
         <div class="info">
             <h2>Slide 05</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
