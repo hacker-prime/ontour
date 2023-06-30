241dd0ce0aca7c9a608d6ac6163de368bd5a0970
hacker-prime <hacker0prime@gmail.com>
2023-06-29 21:16:41 -0500

    Update CSS
    It was changed to match one of the brand colors for the company.

--- signin/handlers/config.php
+++ signin/handlers/config.php
@@ -7,7 +7,10 @@
 	$con = mysqli_connect("localhost", "root", "", "clients");
 	// $con = mysqli_connect("localhost", "u688999092_prime", "Immortal_eternal_infinite_8", "u688999092_phoenix");
 	// $con = mysqli_connect("localhost","my_user","my_password","my_db");
+	// u688999092_clients
 	// c8LHbje?*
+	// u688999092_clients
+
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
