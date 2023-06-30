08fde5cc0dd535033c65a02d5312905b1e201a81
hacker-prime <hacker0prime@gmail.com>
2023-06-29 20:34:14 -0500

    Database Password

--- signin/handlers/config.php
+++ signin/handlers/config.php
@@ -7,7 +7,7 @@
 	$con = mysqli_connect("localhost", "root", "", "clients");
 	// $con = mysqli_connect("localhost", "u688999092_prime", "Immortal_eternal_infinite_8", "u688999092_phoenix");
 	// $con = mysqli_connect("localhost","my_user","my_password","my_db");
-
+	// c8LHbje?*
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
